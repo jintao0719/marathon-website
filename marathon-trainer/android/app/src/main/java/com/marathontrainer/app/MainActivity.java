084ed0e193a925f08f79d90a2993f4c0daef55fd
Jintao <1198448775@qq.com>
2026-02-24 18:42:58 +0800

    添加 Android App 支持 (v0.3.0)

---
+++ marathon-trainer/android/app/src/main/java/com/marathontrainer/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.marathontrainer.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ marathon-trainer/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marathontrainer.app',
+  appName: 'MarathonTrainer',
+  webDir: 'out'
+};
+
+export default config;
